0f8afd8561ba7ace80052f0cc17e6d3ee3db920e
HEEKWON <higun295@gmail.com>
2024-03-26 00:46:46 +0900

    first commit

---
+++ src/Test.java
@@ -0,0 +1,28 @@
+class SuperObject {
+    public void draw() {
+        System.out.println("A");
+        draw();
+    }
+    public void paint() {
+        System.out.println('B');
+        draw();
+    }
+}
+class SubObject extends SuperObject {
+    public void paint() {
+        super.paint();
+        System.out.println('C');
+        draw();
+    }
+    public void draw() {
+        System.out.println('D');
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        SuperObject a = new SubObject();
+        a.paint();
+        a.draw();
+    }
+}
